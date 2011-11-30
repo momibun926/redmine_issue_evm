bb0d7c02b80dd85cf2ffa0ae44b3b307c046d944
Martin Denizet <martin.denizet@supinfo.com>
2011-11-30 15:03:34 +0800

    Version bump.

--- init.rb
+++ init.rb
@@ -11,8 +11,8 @@ end
 Redmine::Plugin.register :redmine_glossary do
   name 'Redmine Glossary Plugin'
   author 'M. Yoshida'
-  description "This is a Redmine plugin to create a glossary which is is a list of terms in a project"
-  version '0.6.1'
+  description "This is a fork of M. Yoshida Redmine plugin. It is used to create a glossary which is is a list of terms in a project"
+  version '0.6.2'
   author_url 'http://yohshiy.blog.fc2.com/'
   url 'http://www.r-labs.org/projects/rp-glossary/wiki/GlossaryEn'
 
