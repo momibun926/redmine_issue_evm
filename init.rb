6183b847592880a0dae706d52e5da7d011dfbf11
Hajime <hajime.nakagama@gmail.com>
2014-11-20 22:44:31 +0900

    initファイルの追記

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_issue_evm do
   name 'Redmine Issue Evm plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author 'Hajime Nakagama'
+  description 'This is a plugin for Redmine. Earned value management using the ticket of redmine.'
+  version '0.1'
+  url 'https://github.com/momibun926/redmine_issue_evm'
+  author_url 'https://github.com/momibun926'
 end
