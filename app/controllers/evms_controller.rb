5a511068740925a095353a7465978b292bca9c6e
Hajime <hajime.nakagama@gmail.com>
2014-11-22 20:41:24 +0900

    主要EVM値の計算ロジック作成

--- app/controllers/evms_controller.rb
+++ app/controllers/evms_controller.rb
@@ -12,10 +12,54 @@ class EvmsController < ApplicationController
   before_filter :find_project, :authorize
 
   def index
+  	actual_pv = actual_issue_pv(@project.id)
+  	ev = issue_ev(@project.id)
+  	ac = issue_ac(@project.id)
+
+  	today_pv_value_hours = today_pv(actual_pv, 1)
+  	today_pv_value_days = today_pv(actual_pv, 8)
+
+  	today_ev_value_hours = today_ev(ev, 1)
+  	today_ev_value_days = today_ev(ev, 8)
+
+  	today_ac_value_hours = today_ac(ac, 1)
+  	today_ac_value_days = today_ac(ac, 8)
+
   	@chart_data = {}
-    @chart_data['planned_value'] = convert_to_chart(actual_issue_pv(@project.id))
-    @chart_data['earned_value'] = convert_to_chart(issue_ev(@project.id))
-    @chart_data['actual_cost'] = convert_to_chart(issue_ac(@project.id))
+    @chart_data['planned_value'] = convert_to_chart(actual_pv)
+    @chart_data['earned_value'] = convert_to_chart(ev)
+    @chart_data['actual_cost'] = convert_to_chart(ac)
+    #Tody's value
+  	@todays_value = {}
+  	@todays_value['complete_ev_value'] = complete_ev(ev, actual_pv)
+
+  	@todays_value['bac_value_hours'] = bac(actual_pv, 1)
+  	@todays_value['bac_value_days'] = bac(actual_pv, 8)
+
+  	@todays_value['todays_pv_value_hours'] = today_pv_value_hours
+  	@todays_value['todays_pv_value_days'] = today_pv_value_days
+
+  	@todays_value['todays_ev_value_hours'] = today_ev_value_hours
+  	@todays_value['todays_ev_value_days'] = today_ev_value_days
+
+  	@todays_value['todays_ac_value_hours'] = today_ac_value_hours
+  	@todays_value['todays_ac_value_days'] = today_ac_value_days
+
+  	@todays_value['todays_sv_value_hours'] = today_sv(today_ev_value_hours, today_pv_value_hours)
+  	@todays_value['todays_sv_value_days'] = today_sv(today_ev_value_days, today_pv_value_days)
+
+  	@todays_value['todays_cv_value_hours'] = today_cv(today_ev_value_hours, today_ac_value_hours)
+  	@todays_value['todays_cv_value_days'] = today_cv(today_ev_value_days, today_ac_value_days)
+
+  	@todays_value['todays_spi_value_hours'] = today_spi(today_ev_value_hours, today_pv_value_hours)
+  	@todays_value['todays_spi_value_days'] = today_spi(today_ev_value_days, today_pv_value_days)
+
+  	@todays_value['todays_cpi_value_hours'] = today_cpi(today_ev_value_hours, today_ac_value_hours)
+  	@todays_value['todays_cpi_value_days'] = today_cpi(today_ev_value_days, today_ac_value_days)
+  	
+  	@todays_value['todays_cr_value_hours'] = today_cr(today_ev_value_hours, today_ac_value_hours, today_pv_value_hours)
+  	@todays_value['todays_cr_value_days'] = today_cr(today_ev_value_days, today_ac_value_days, today_pv_value_days)
+    
   end
 
   def show
--- lib/evm_logic.rb
+++ lib/evm_logic.rb
@@ -15,6 +15,7 @@ module EvmLogic
         temp_pv[key].nil? ? temp_pv[key] = hours_per_day : temp_pv[key] += hours_per_day
       end
     end
+    # Sort by key
     actual_pv = sort_evm_hash(temp_pv)
   end
 
@@ -30,20 +31,26 @@ module EvmLogic
           close_date = issue.closed_on.to_date
           temp_ev[close_date].nil? ? temp_ev[close_date] = issue.estimated_hours : temp_ev[close_date] += issue.estimated_hours
       else
+        #進捗率は入力されていたら出来高計算
         if issue.done_ratio > 0
-          #一日当たりの時間
-          hours_per_day = issue_hours_per_day(issue.estimated_hours ,issue.due_date, issue.start_date) * (issue.done_ratio / 100)
-        else
-          #一日当たりの時間
-          hours_per_day = issue_hours_per_day(issue.estimated_hours ,issue.due_date, issue.start_date)
-        end
-        #チケットの期間の日毎に一日当たりの時間を加算
-        (issue.start_date..issue.due_date).each do |key|
-          temp_ev[key].nil? ? temp_ev[key] = hours_per_day : temp_ev[key] += hours_per_day
+          if Time.now.to_date < issue.start_date
+            #開始日前だったら見積もり工数*進捗率を今日日付で計上
+            temp_ev[Time.now.to_date] = issue.estimated_hours * (issue.done_ratio / 100.0)
+          else
+            #一日当たりの時間
+            hours_per_day = issue_hours_per_day(issue.estimated_hours ,issue.due_date, issue.start_date) * (issue.done_ratio / 100.0)
+            #チケットの期間の日毎に一日当たりの時間を加算
+            (issue.start_date..issue.due_date).each do |key|
+              temp_ev[key].nil? ? temp_ev[key] = hours_per_day : temp_ev[key] += hours_per_day
+            end 
+          end 
         end
       end
     end
+    # Sort by key
     issue_ev = sort_evm_hash(temp_ev)
+    #今日以降のEVは削除
+    issue_ev.delete_if{|key, value| key > Time.now.to_date }
   end
 
   def issue_ac project_id
@@ -52,7 +59,58 @@ module EvmLogic
             joins(:time_entries).
             group('spent_on').collect { |issue| [issue.spent_on, issue.sum_hours] }
     temp_ac = Hash[query]
+    #　sort by key
     issue_ac = sort_evm_hash(temp_ac)
+  end
+
+  def bac issue_pv_hash, hours
+    bac = issue_pv_hash[issue_pv_hash.keys.max] / hours
+    bac.round(2)
+  end
+
+  def complete_ev issue_ev_hash, issue_pv_hash
+    complete_ev = today_ev(issue_ev_hash, 1) / bac(issue_pv_hash, 1)
+    complete_ev.round(2)
+  end
+
+  def today_pv issue_pv_hash, hours
+    pv = issue_pv_hash[Time.now.to_date] / hours
+    pv.round(2)
+  end
+
+  def today_ev issue_ev_hash, hours
+    ev = issue_ev_hash[Time.now.to_date] / hours
+    ev.round(2)
+  end
+  
+  def today_ac issue_ac_hash, hours
+    ac = issue_ac_hash[Time.now.to_date] / hours
+    ac.round(2)
+  end
+
+  def today_sv ev, pv
+    sv = (ev - pv)
+    sv.round(2)
+  end
+
+  def today_cv ev, ac
+    cv = (ev - ac)
+    cv.round(2)
+  end
+
+  def today_spi ev, pv
+    spi = ev / pv
+    spi.round(2)
+  end
+
+  def today_cpi ev, ac
+    cpi = ev / ac
+    cpi.round(2)
+  end
+
+  def today_cr ev, ac, pv
+    cr = today_spi(ev, pv) * today_cpi(ev, ac)
+    cr.round(2)
   end
 
   #チャート用データの加工
@@ -65,13 +123,22 @@ module EvmLogic
   def sort_evm_hash evm_hash 
     temp_hash = {}
     sum_value = 0
+    #今日の値がなかったら0をセット
+    if evm_hash[Time.now.to_date].nil?
+      evm_hash[Time.now.to_date] = 0
+    end
     evm_hash.sort_by{|key,val| key}.each do |date , value|
       sum_value += value
       temp_hash[date] = sum_value
     end
+    #今日が最大日を超えていたら、今日に最大値をセット
+    if temp_hash.keys.max < Time.now.to_date
+      temp_hash[Time.now.to_date] = temp_hash[temp_hash.keys.max]
+    end
     temp_hash
   end
-
+  
+  #一日当たりの見積もり時間を算出
   def issue_hours_per_day estimated_hours, due_date, start_date
     estimated_hours / ((due_date + 1) - start_date)
   end
