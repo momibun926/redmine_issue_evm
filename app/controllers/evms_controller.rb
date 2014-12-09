ec6425b28af251de9db96d398e4c434720ca8d1f
Hajime <hajime.nakagama@gmail.com>
2014-12-09 21:30:31 +0900

    refactor

--- app/controllers/evms_controller.rb
+++ app/controllers/evms_controller.rb
@@ -29,16 +29,16 @@ class EvmsController < ApplicationController
     #Project. all versions
     baselines = project_baseline @project, params[:evmbaseline_id]
     issues = project_issues @project
-    costs = project_costs @project
-    @project_evm = IssueEvm.new(baselines, issues, costs, Time.now.to_date, params[:forecast], params[:calcetc], params[:actual_basis])
+    actual_cost = project_costs @project
+    @project_evm = IssueEvm.new(baselines, issues, actual_cost, Time.now.to_date, params[:forecast], params[:calcetc], params[:actual_basis])
 
     #versions
     @version_evm = {}
     unless @project.versions.nil?
       @project.versions.each do |version|
         issues = version_issues @project, version.id
-        costs = version_costs @project, version.id
-        @version_evm[version.id] = IssueEvm.new(nil, issues, costs, Time.now.to_date, nil, nil, true)
+        actual_cost = version_costs @project, version.id
+        @version_evm[version.id] = IssueEvm.new(nil, issues, actual_cost, Time.now.to_date, nil, nil, true)
       end
     end 
 
