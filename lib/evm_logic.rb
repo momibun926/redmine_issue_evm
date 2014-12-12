07aa162df78def8d52a976414cb3230021b4308f
Hajime <hajime.nakagama@gmail.com>
2014-12-12 22:39:33 +0900

    change variables name , method name calculate earned value is using actual start-date,end-date

--- lib/evm_logic.rb
+++ lib/evm_logic.rb
@@ -4,10 +4,10 @@ module EvmLogic
 
     def initialize baselines, issues, costs, basis_date, forecast, etc_method, calc_basis_actual, performance
       @basis_date = basis_date
+      #option
       @forecast = forecast
       @etc_method = etc_method
       @performance = performance
-
       #PV-ACTUAL
       @pv_actual = calculate_planed_value issues
       #PV-BASELINE
@@ -18,7 +18,7 @@ module EvmLogic
       @ev = calculate_earned_value issues
       #AC
       @ac = calculate_actual_cost costs
-
+      #Project is finish?
       if @basis_date > @pv.keys.max && @pv[@pv.keys.max] != @ev[@ev.keys.max]
         @pv[@basis_date] = @pv[@pv.keys.max]
         @ev[@basis_date] = @ev[@ev.keys.max]
@@ -26,6 +26,7 @@ module EvmLogic
     end
 
 
+    #Basis date
     def basis_date
       @basis_date
     end
@@ -108,14 +109,15 @@ module EvmLogic
       else
         case @etc_method
         when 'method1' then
-          etc = (bac(hours) - today_ev(hours))
+          div_value = 1.0
         when 'method2' then
-          etc = (bac(hours) - today_ev(hours)) / today_cpi(hours)
+          div_value = today_cpi(hours)
         when 'method3' then
-          etc = (bac(hours) - today_ev(hours)) / today_cr(hours)
+          div_value = today_cr(hours)
         else
-          etc = (bac(hours) - today_ev(hours)) / today_cpi(hours)
+          div_value = today_cpi(hours)
         end
+        etc = (bac(hours) - today_ev(hours)) / div_value
       end
       etc.round(2)
     end
@@ -134,11 +136,9 @@ module EvmLogic
       vac.round(2)
     end
 
-
+    #Delay
     def delay
-      unless forecast_finish_date.nil?
-        (forecast_finish_date - @pv.keys.max).to_i
-      end 
+      (forecast_finish_date - @pv.keys.max).to_i
     end
 
 
@@ -152,65 +152,55 @@ module EvmLogic
     #Create chart data
     def chart_data
       chart_date = {}
-
       chart_date['planned_value'] = convert_to_chart(@pv_actual)
       chart_date['actual_cost'] = convert_to_chart(@ac)
       chart_date['earned_value'] = convert_to_chart(@ev)
       chart_date['baseline_value'] = convert_to_chart(@pv_baseline)
-
       if @forecast
         bac_top_line = {chrat_minimum_date => bac(1), chrat_maximum_date => bac(1)}
         chart_date['bac_top_line'] = convert_to_chart(bac_top_line)
-
         eac_top_line = {chrat_minimum_date => eac(1), chrat_maximum_date => eac(1)}
         chart_date['eac_top_line'] = convert_to_chart(eac_top_line)
-
         actual_cost_forecast = {@basis_date => today_ac(1), forecast_finish_date => eac(1)}
         chart_date['actual_cost_forecast'] = convert_to_chart(actual_cost_forecast)
-
         earned_value_forecast = {@basis_date => today_ev(1), forecast_finish_date => @pv[@pv.keys.max]}
         chart_date['earned_value_forecast'] = convert_to_chart(earned_value_forecast)
       end
-
       if @performance
-        calc_performance
+        create_performance_index_hash
         chart_date['spi'] = convert_to_chart(@spi)
         chart_date['cpi'] = convert_to_chart(@cpi)
         chart_date['cr'] = convert_to_chart(@cr)
       end 
-
       chart_date
     end
 
 
-    def calc_performance
+    def create_performance_index_hash
       @spi = {}
       @cpi = {}
       @cr = {}
-
-      new_ev = complement_evm_hash @ev
-      new_ac = complement_evm_hash @ac
-      new_pv = complement_evm_hash @pv
-
+      new_ev = complement_evm_value @ev
+      new_ac = complement_evm_value @ac
+      new_pv = complement_evm_value @pv
       new_ev.each do |date , value|
         @spi[date] = (value / new_pv[date]).round(2) unless new_pv[date].nil? 
         @cpi[date] = (value / new_ac[date]).round(2) unless new_ac[date].nil? 
         @cr[date] = (@spi[date] * @cpi[date]).round(2)
       end
-
     end
 
 
-  private
+    private
 
       def calculate_planed_value issues
         temp_pv = {}
         unless issues.nil?
           issues.each do |issue|
             next unless issue.leaf?
-            hours_per_day = issue_hours_per_day(issue)
-            (issue.start_date..issue.due_date).each do |key|
-              temp_pv[key].nil? ? temp_pv[key] = hours_per_day : temp_pv[key] += hours_per_day
+            hours_per_day = issue_hours_per_day(issue.estimated_hours, issue.start_date, issue.due_date)
+            (issue.start_date..issue.due_date).each do |date|
+              temp_pv[date].nil? ? temp_pv[date] = hours_per_day : temp_pv[date] += hours_per_day
             end
           end
         end 
@@ -225,25 +215,22 @@ module EvmLogic
           issues.each do |issue|
             next unless issue.leaf?
             if issue.closed?
-                close_date = issue.closed_on.utc.to_date
-                temp_ev[close_date].nil? ? temp_ev[close_date] = issue.estimated_hours : temp_ev[close_date] += issue.estimated_hours.to_f
-            else
-              if issue.done_ratio > 0
-                if @basis_date < issue.start_date
-                  temp_ev[@basis_date] = (issue.estimated_hours * (issue.done_ratio / 100.0)).to_f
-                else
-                  hours_per_day = issue_hours_per_day(issue) * (issue.done_ratio / 100.0)
-                  (issue.start_date..issue.due_date).each do |key|
-                    temp_ev[key].nil? ? temp_ev[key] = hours_per_day : temp_ev[key] += hours_per_day
-                  end 
-                end 
-              end
+              close_date = issue.closed_on.utc.to_date
+              temp_ev[close_date].nil? ? temp_ev[close_date] = issue.estimated_hours : temp_ev[close_date] += issue.estimated_hours.to_f
+            elsif issue.done_ratio > 0
+              estimated_hours = issue.estimated_hours * issue.done_ratio / 100.0
+              start_date = [issue.start_date, @basis_date].min
+              end_date = [issue.due_date, @basis_date].max
+              hours_per_day = issue_hours_per_day(estimated_hours, start_date, end_date)
+              (start_date..end_date).each do |date|
+                temp_ev[date].nil? ? temp_ev[date] = hours_per_day : temp_ev[date] += hours_per_day
+              end 
             end
           end
         end
         # Sort and sum value
         calculate_earned_value = sort_and_sum_evm_hash(temp_ev)
-        calculate_earned_value.delete_if{|key, value| key > @basis_date }
+        calculate_earned_value.delete_if{|date, value| date > @basis_date }
       end
 
 
@@ -264,9 +251,7 @@ module EvmLogic
       def sort_and_sum_evm_hash evm_hash 
         temp_hash = {}
         sum_value = 0.0
-        if evm_hash.nil? || evm_hash[@basis_date].nil?
-          evm_hash[@basis_date] = 0.0
-        end        
+        evm_hash[@basis_date] = 0.0 if evm_hash.nil? || evm_hash[@basis_date].nil?
         evm_hash.sort_by{|key,val| key}.each do |date , value|
           sum_value += value
           temp_hash[date] = sum_value
@@ -275,8 +260,8 @@ module EvmLogic
       end
     
 
-      def issue_hours_per_day issue
-        (issue.estimated_hours / (issue.due_date - issue.start_date + 1) ).to_f
+      def issue_hours_per_day estimated_hours, start_date, end_date
+        (estimated_hours / (end_date - start_date + 1)).to_f
       end
 
 
@@ -297,14 +282,12 @@ module EvmLogic
           rest_days =  @pv.reject{|key, value| key <= @basis_date }.size
           finish_date = @pv.keys.max - (rest_days - (rest_days / today_spi(8)).round )
         end
-
       end
 
 
-      def complement_evm_hash evm_hash
+      def complement_evm_value evm_hash
         before_date = evm_hash.keys.min
         before_value = evm_hash[evm_hash.keys.min]
-
         temp = {}
         evm_hash.each do |date , value|
           dif_days = ( date - before_date -1 ).to_i
@@ -321,7 +304,7 @@ module EvmLogic
           before_value = value
           temp[date] = value
         end
-        return temp
+        temp
       end
 
   end
