09c47bf71d6fed479bd70c457bd203bebefb6994
Hajime <hajime.nakagama@gmail.com>
2014-12-22 01:19:51 +0900

    refactor

--- app/controllers/evms_controller.rb
+++ app/controllers/evms_controller.rb
@@ -11,14 +11,14 @@ class EvmsController < ApplicationController
 
   def index
     # Basis date of calculate
-    @basis_date = params[:basis_date].nil? ? Time.now.to_date : params[:basis_date].to_date
+    @basis_date = default_basis_date
     # baseline combo
-    @evmbaseline = Evmbaseline.where('project_id = ? ', @project.id).order('created_on DESC')
+    @evmbaseline = find_evmbaselines
     # option parameters
-    @baseline_id = params[:evmbaseline_id].nil? ? nil : params[:evmbaseline_id]
-    @actual_basis = @evmbaseline.blank? ? 'ture' : params[:actual_basis]
+    @baseline_id = default_baseline_id
+    @actual_basis = default_actual_basis
+    @calcetc = default_calcetc
     @forecast = params[:forecast]
-    @calcetc = params[:calcetc].nil? ? 'method2' : params[:calcetc]
     @display_explanation = params[:display_explanation]
     @display_version = params[:display_version]
     @display_performance = params[:display_performance]
@@ -31,20 +31,53 @@ class EvmsController < ApplicationController
     unless @project.versions.nil?
       @version_evm = {}
       @project.versions.each do |version|
-        version_issues = version_issues @project, version.id
+        version_issue = version_issues @project, version.id
         version_actual_cost = version_costs @project, version.id
-        @version_evm[version.id] = IssueEvm.new(nil, version_issues, version_actual_cost, @basis_date, nil, nil, true, nil)
+        @version_evm[version.id] = IssueEvm.new(nil, version_issue, version_actual_cost, @basis_date, nil, nil, true, nil)
       end
     end
     @no_data = issues.blank?
 
   end
 
+
 private
+
+
+  def default_basis_date
+    params[:basis_date].nil? ? Time.now.to_date : params[:basis_date].to_date
+  end
+
+
+  def default_baseline_id
+    if params[:evmbaseline_id].nil?
+      id = @evmbaseline.blank? ? nil : @evmbaseline.first.id
+    else
+      id = params[:evmbaseline_id]
+    end
+  end
+
+
+  def default_actual_basis
+    @evmbaseline.blank? ? 'ture' : params[:actual_basis]
+  end
+
+
+  def default_calcetc
+    params[:calcetc].nil? ? 'method2' : params[:calcetc]
+  end
+
+
   def find_project
     @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
     render_404
   end
+
+
+  def find_evmbaselines
+    Evmbaseline.where('project_id = ? ', @project.id).order('created_on DESC')
+  end
+
 
 end
