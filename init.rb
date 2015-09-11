0a20f9a18cceedc8de41fd4d42ab125e3b65add2
Hajime <hajime.nakagama@gmail.com>
2015-09-12 07:49:59 +0900

    update to tab-name

--- init.rb
+++ init.rb
@@ -1,19 +1,19 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_issue_evm do
-	
+
   name 'Redmine Issue Evm plugin'
   author 'Hajime Nakagama'
   description 'This is a plugin for Redmine. Earned value management using the ticket of redmine.'
   version '3.1'
   url 'https://github.com/momibun926/redmine_issue_evm'
   author_url 'https://github.com/momibun926'
-  
+
   project_module :Issuevm do
     permission :view_evms, :evms => :index , :require => :member
-    permission :manage_evms, { :evmbaselines => [:edit, :destroy, :new, :create, :update, :index] } 
+    permission :manage_evms, { :evmbaselines => [:edit, :destroy, :new, :create, :update, :index] }
   end
 
-  menu :project_menu, :issuevm, { :controller => :evms, :action => :index}, :caption => 'IssueEVM', :param => :project_id
+  menu :project_menu, :issuevm, { :controller => :evms, :action => :index}, :caption => :tab_display_name, :param => :project_id
 
 end
