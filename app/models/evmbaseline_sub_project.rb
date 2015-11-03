06ecb5b4e035baaabd5e85d17a5c27db97ec1af8
Hajime <hajime.nakagama@gmail.com>
2015-11-03 12:34:01 +0900

    code refactor and modify readme(jp)

--- app/models/evmbaseline_sub_project.rb
+++
@@ -1,8 +0,0 @@
-class EvmbaselineSubProject < ActiveRecord::Base
-  unloadable
-
-  attr_protected :id
-
-  belongs_to :evmbaseline
-
-end
--- db/migrate/003_create_evmbaseline_sub_projects.rb
+++
@@ -1,8 +0,0 @@
-class CreateEvmbaselineSubProjects < ActiveRecord::Migration
-  def change
-    create_table :evmbaseline_sub_projects do |t|
-      t.integer :evmbaseline_id
-      t.integer :sub_project_id
-    end
-  end
-end
