5f5dff877da6ed3554be3d026ce3fb25de659d27
Hajime <hajime.nakagama@gmail.com>
2015-11-21 18:42:24 +0900

    default value.

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_issue_evm do
   name 'Redmine Issue Evm plugin'
   author 'Hajime Nakagama'
   description 'This is a plugin for Redmine. Earned value management using the ticket of redmine.'
-  version '3.4'
+  version '3.4.1'
   url 'https://github.com/momibun926/redmine_issue_evm'
   author_url 'https://github.com/momibun926'
 
@@ -16,6 +16,10 @@ Redmine::Plugin.register :redmine_issue_evm do
 
   menu :project_menu, :issuevm, { :controller => :evms, :action => :index}, :caption => :tab_display_name, :param => :project_id
 
-  settings :default => {'working_hours_of_day' => '8.0'}, :partial => 'settings/issue_evm_settings'
+  settings :default => {'working_hours_of_day' => '8.0',
+                        'limit_spi' => '0.90',
+                        'limit_cpi' => '0.90',
+                        'limit_cr' => '0.80'},
+           :partial => 'settings/issue_evm_settings'
 
 end
