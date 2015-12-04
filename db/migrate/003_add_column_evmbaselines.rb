bb3ccefa6129e105fb7a5935ecc661bd2987ad62
Hajime <hajime.nakagama@gmail.com>
2015-12-05 08:06:27 +0900

    mistake

--- db/migrate/003_add_column_evmbaselines.rb
+++ db/migrate/003_add_column_evmbaselines.rb
@@ -1,7 +1,7 @@
 class AddColumnEvmbaselines < ActiveRecord::Migration
   def change
-    add_column :Evmbaselines :author_id, :integer
-    add_column :Evmbaselines :update_id, :integer
-    add_column :Evmbaselines :updated_on, :datetime
+    add_column :Evmbaselines, :author_id, :integer
+    add_column :Evmbaselines, :update_id, :integer
+    add_column :Evmbaselines, :updated_on, :datetime
   end
 end
