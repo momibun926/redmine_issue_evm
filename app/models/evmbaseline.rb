84dfc038a9e52dbd83b62f65e2aa72aa6c831342
Hajime <hajime.nakagama@gmail.com>
2015-12-05 18:24:46 +0900

    Chage permission of baseline

--- app/models/evmbaseline.rb
+++ app/models/evmbaseline.rb
@@ -2,8 +2,11 @@ class Evmbaseline < ActiveRecord::Base
   unloadable
 
   attr_protected :id
+
   has_many :evmbaselineIssues, :dependent => :delete_all
   belongs_to :author, :class_name => 'User'
+  belongs_to :project
+
   validates :subject, :presence => true
 
   def minimum_start_date
@@ -17,5 +20,15 @@ class Evmbaseline < ActiveRecord::Base
   def bac
     evmbaselineIssues.sum(:estimated_hours).round(1)
   end
+
+  #acts_as_event :title => 'test',
+  #              :description => :subject,
+  #              :datetime => :created_on,
+  #              :type => Proc.new { |o| 'evmBaseine-' + (o.created_on < o.updated_on ? 'edit' : 'new') },
+  #              :url => Proc.new { |o| {:controller => 'EvmbaselinesController', :action => 'index', :id => o.id, :project_id => o.project_id} }
+
+  #acts_as_activity_provider :scope => preload(:projects),
+  #                          :author_key => :author_id,
+  #                          :timestamp => :created_on
 
 end
--- init.rb
+++ init.rb
@@ -1,21 +1,29 @@
 require 'redmine'
 
+#Redmine::Activity.register :evmbaseline
+
 Redmine::Plugin.register :redmine_issue_evm do
 
   name 'Redmine Issue Evm plugin'
   author 'Hajime Nakagama'
   description 'This is a plugin for Redmine. Earned value management using the ticket of redmine.'
-  version '3.4.1'
+  version '3.5'
   url 'https://github.com/momibun926/redmine_issue_evm'
   author_url 'https://github.com/momibun926'
 
   project_module :Issuevm do
     permission :view_evms, :evms => :index , :require => :member
-    permission :manage_evms, { :evmbaselines => [:edit, :destroy, :new, :create, :update, :index] }
+    permission :manage_evmbaseline, { :evmbaselines => [:edit, :destroy, :new, :create, :update, :index] }
+    permission :view_evmbaseline, { :evmbaselines => [:index] }
   end
 
   menu :project_menu, :issuevm, { :controller => :evms, :action => :index}, :caption => :tab_display_name, :param => :project_id
 
-  settings :default => {'working_hours_of_day' => '8.0','limit_spi' => '0.9','limit_cpi' => '0.9','limit_cr' => '0.8'}, :partial => 'settings/issue_evm_settings'
+  settings :default => {'working_hours_of_day' => '8.0',
+                        'limit_spi' => '0.9',
+                        'limit_cpi' => '0.9',
+                        'limit_cr' => '0.8'},
+                       :partial => 'settings/issue_evm_settings'
+
 
 end
