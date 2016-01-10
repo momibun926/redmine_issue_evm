956dc436d450de41bd93e2d6741a657b5eed0045
Hajime <hajime.nakagama@gmail.com>
2016-01-11 00:14:12 +0900

    Change single quote to double quote.

--- lib/project_and_version_value.rb
+++ lib/project_and_version_value.rb
@@ -10,9 +10,9 @@ module ProjectAndVersionValue
     baselines = {}
     if Evmbaseline.exists?(project_id: project_id)
       if baseline_id.nil?
-        baselines = Evmbaseline.where('project_id = ? ', project_id).order('created_on DESC').first.evmbaselineIssues
+        baselines = Evmbaseline.where("project_id = ? ", project_id).order("created_on DESC").first.evmbaselineIssues
       else
-        baselines = Evmbaseline.where('id = ? ', baseline_id).first.evmbaselineIssues
+        baselines = Evmbaseline.where("id = ? ", baseline_id).first.evmbaselineIssues
       end
     end
     baselines
@@ -24,8 +24,8 @@ module ProjectAndVersionValue
   # @param [Object] proj project
   # @return [Issue] issue object
   def project_issues(proj)
-    Issue.cross_project_scope(proj, 'descendants')
-      .where('start_date IS NOT NULL AND due_date IS NOT NULL')
+    Issue.cross_project_scope(proj, "descendants")
+      .where("start_date IS NOT NULL AND due_date IS NOT NULL")
   end
 
   # Get spent time of project.
@@ -34,9 +34,9 @@ module ProjectAndVersionValue
   # @param [Object] proj project
   # @return [Array] Two column,spent_on,sum of hours
   def project_costs(proj)
-    Issue.cross_project_scope(proj, 'descendants')
-      .select('MAX(spent_on) AS spent_on, SUM(hours) AS sum_hours')
-      .where('start_date IS NOT NULL AND due_date IS NOT NULL')
+    Issue.cross_project_scope(proj, "descendants")
+      .select("MAX(spent_on) AS spent_on, SUM(hours) AS sum_hours")
+      .where("start_date IS NOT NULL AND due_date IS NOT NULL")
       .joins(:time_entries)
       .group(:spent_on).collect { |issue| [issue.spent_on, issue.sum_hours] }
   end
@@ -49,8 +49,8 @@ module ProjectAndVersionValue
   # @return [Issue] issue object
   def version_issues(proj_id, version_id)
     proj = Project.find(proj_id)
-    Issue.cross_project_scope(proj, 'descendants')
-      .where('start_date IS NOT NULL AND due_date IS NOT NULL AND fixed_version_id = ? ', version_id)
+    Issue.cross_project_scope(proj, "descendants")
+      .where("start_date IS NOT NULL AND due_date IS NOT NULL AND fixed_version_id = ? ", version_id)
   end
 
   # Get spent time of version.
@@ -61,9 +61,9 @@ module ProjectAndVersionValue
   # @return [Issue] Two column,spent_on,sum of hours
   def version_costs(proj_id, version_id)
     proj = Project.find(proj_id)
-    Issue.cross_project_scope(proj, 'descendants')
-      .select('MAX(spent_on) AS spent_on, SUM(hours) AS sum_hours')
-      .where('start_date IS NOT NULL AND due_date IS NOT NULL AND fixed_version_id = ? ', version_id)
+    Issue.cross_project_scope(proj, "descendants")
+      .select("MAX(spent_on) AS spent_on, SUM(hours) AS sum_hours")
+      .where("start_date IS NOT NULL AND due_date IS NOT NULL AND fixed_version_id = ? ", version_id)
       .joins(:time_entries)
       .group(:spent_on).collect { |issue| [issue.spent_on, issue.sum_hours] }
   end
@@ -74,8 +74,8 @@ module ProjectAndVersionValue
   # @param [date] basis_date basis date
   # @return [Issue] issue object
   def incomplete_project_issues(proj, basis_date)
-    Issue.cross_project_scope(proj, 'descendants')
-      .where('start_date IS NOT NULL AND start_date <= ? AND due_date IS NOT NULL AND (closed_on IS NULL OR closed_on > ?)', basis_date, basis_date.end_of_day)
+    Issue.cross_project_scope(proj, "descendants")
+      .where("start_date IS NOT NULL AND start_date <= ? AND due_date IS NOT NULL AND (closed_on IS NULL OR closed_on > ?)", basis_date, basis_date.end_of_day)
   end
 
   # Get pair of project id and fixed version id.
@@ -83,9 +83,9 @@ module ProjectAndVersionValue
   # @param [project] proj project object
   # @return [Array] project_id, fixed_version_id
   def project_varsion_id_pair(proj)
-    Issue.cross_project_scope(proj, 'descendants')
-      .where('start_date IS NOT NULL AND due_date IS NOT NULL AND fixed_version_id IS NOT NULL')
-      .joins(:fixed_version).order('effective_date ASC')
+    Issue.cross_project_scope(proj, "descendants")
+      .where("start_date IS NOT NULL AND due_date IS NOT NULL AND fixed_version_id IS NOT NULL")
+      .joins(:fixed_version).order("effective_date ASC")
       .uniq.pluck(:project_id, :fixed_version_id)
   end
 end
