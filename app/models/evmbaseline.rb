1f71d4b0415277f7c2d30b6940df6a40a4cbd8c6
Hajime <hajime.nakagama@gmail.com>
2017-04-05 08:29:23 +0900

    # 77

--- app/models/evmbaseline.rb
+++ app/models/evmbaseline.rb
@@ -35,8 +35,10 @@ class Evmbaseline < ActiveRecord::Base
   end
 
   # for activity page.
-  acts_as_event title: Proc.new { |o| l(:title_evm_tab) + ' : ' + o.subject },
-                description: Proc.new { |o| (o.created_on < o.updated_on ? l(:label_ativity_message_edit) : l(:label_ativity_message_new)) },
+  acts_as_event title: Proc.new { |o| l(:title_evm_tab) + ' : ' +
+                                      o.subject + ' : ' +
+                                      (o.created_on < o.updated_on ? l(:label_ativity_message_edit) : l(:label_ativity_message_new))},
+                description: :description,
                 datetime: :updated_on,
                 type: Proc.new { |o| 'evmbaseline-' + (o.created_on < o.updated_on ? 'edit' : 'new') },
                 url: Proc.new { |o| { controller: 'evmbaselines', action: :show, project_id: o.project, id: o.id } }
