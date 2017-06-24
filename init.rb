84b01756fff33fdbee7f5d46fa5cb852d0946204
momibun926 <hajime.nakagama@gmail.com>
2017-06-24 11:37:14 +0900

    #89 close and verup.

--- init.rb
+++ init.rb
@@ -17,7 +17,7 @@ Redmine::Plugin.register :redmine_issue_evm do
   name 'Redmine Issue Evm plugin'
   author 'Hajime Nakagama'
   description 'Earned value management calculation plugin.'
-  version '3.9.2'
+  version '3.9.3'
   url 'https://github.com/momibun926/redmine_issue_evm'
   author_url 'https://github.com/momibun926'
 
