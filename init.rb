6b3a6d9d070f809aea74836f877cd5eb084bbd01
Toru Takahashi <torutk@gmail.com>
2018-04-30 21:34:14 +0900

    redmine plguin templates, modified init.rb

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_glossary do
+  name 'Redmine Glossary plugin'
+  author 'Toru Takahashi'
+  description 'This is a plugin for Redmine to create a glossary that is a list of terms in a project.'
+  version '1.0.1'
+  url 'https://github.com/torutk/redmine_glossary'
+  author_url 'http://www.torutk.com'
+end
