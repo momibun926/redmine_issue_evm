f484935c370aa25aa300fcfd6ba4dd75085ab4c0
momibun926 <hajime.nakagama@gmail.com>
2018-12-31 15:59:41 +0900

    added setting save.

--- app/controllers/evms_controller.rb
+++ app/controllers/evms_controller.rb
@@ -14,76 +14,79 @@ class EvmsController < ApplicationController
   #
   def index
     # check view setting
-    exist_setting = Evmsetting.where('project_id = ? ', @project.id)
-    if exist_setting.nil? then
-      redirect_to new_evmsetting_path
+    emv_setting = Evmsetting.find_by(:project_id => @project.id)
+    if emv_setting.present? then
+      # plugin setting chart
+      @forecast = emv_setting.view_forecast
+      @display_version = emv_setting.view_version
+      @display_performance = emv_setting.view_performance
+      @display_incomplete = emv_setting.view_issuelist
+      # plugin setting calculation evm
+      @calcetc = default_calcetc
+      @working_hours = emv_setting.basis_hours
+      @limit_spi = emv_setting.threshold_spi
+      @limit_cpi = emv_setting.threshold_cpi
+      @limit_cr = emv_setting.threshold_cr
+      # plugin setting holyday region
+      @region = emv_setting.region
+      # Basis date of calculate
+      @basis_date = default_basis_date
+      # baseline combo
+      @evmbaseline = find_evmbaselines
+      # option parameters
+      @baseline_id = default_baseline_id
+      @no_use_baseline = default_no_use_baseline
+      @display_explanation = params[:display_explanation]
+      # Project. all versions
+      baselines = project_baseline @project, @baseline_id
+      issues = project_issues @project
+      actual_cost = project_costs @project
+      # incomplete issues
+      @incomplete_issues = incomplete_project_issues @project, @basis_date
+      # EVM of project
+      @project_evm = IssueEvm.new baselines,
+                                  issues,
+                                  actual_cost,
+                                  basis_date: @basis_date,
+                                  forecast: @forecast,
+                                  etc_method: @calcetc,
+                                  no_use_baseline: @no_use_baseline,
+                                  working_hours: @working_hours,
+                                  region: @region
+      # EVM of versions
+      @version_evm = {}
+      project_version_ids = project_varsion_id_pair @project
+      unless project_version_ids.nil?
+        project_version_ids.each do |proj_id, ver_id|
+          version_issue = version_issues proj_id,
+                                         ver_id
+          version_actual_cost = version_costs proj_id,
+                                              ver_id
+          @version_evm[ver_id] = IssueEvm.new nil,
+                                              version_issue,
+                                              version_actual_cost,
+                                              basis_date: @basis_date,
+                                              forecast: nil,
+                                              etc_method: nil,
+                                              no_use_baseline: true,
+                                              working_hours: @working_hours,
+                                              region: @region
+        end
+      end
+      @no_data = issues.blank?
+      @no_data_incomplete_issues = @incomplete_issues.blank?
+      # export
+      respond_to do |format|
+        format.html
+        format.csv do
+          send_data @project_evm.to_csv,
+                    type: 'text/csv; header=present',
+                    filename: "evm_#{@project.name}_#{Date.current}.csv"
+        end
+      end
     else
-    end
-    # plugin setting
-    @working_hours = default_setting('working_hours_of_day', 7.5).to_f
-    @limit_spi = default_setting('limit_spi', 0.9).to_f
-    @limit_cpi = default_setting('limit_cpi', 0.9).to_f
-    @limit_cr = default_setting('limit_cr', 0.8).to_f
-    @region = default_setting('holiday_region',:jp)
-    # Basis date of calculate
-    @basis_date = default_basis_date
-    # baseline combo
-    @evmbaseline = find_evmbaselines
-    # option parameters
-    @baseline_id = default_baseline_id
-    @no_use_baseline = default_no_use_baseline
-    @calcetc = default_calcetc
-    @forecast = params[:forecast]
-    @display_explanation = params[:display_explanation]
-    @display_version = params[:display_version]
-    @display_performance = params[:display_performance]
-    @display_incomplete = params[:display_incomplete]
-    # Project. all versions
-    baselines = project_baseline @project, @baseline_id
-    issues = project_issues @project
-    actual_cost = project_costs @project
-    # incomplete issues
-    @incomplete_issues = incomplete_project_issues @project, @basis_date
-    # EVM of project
-    @project_evm = IssueEvm.new baselines,
-                                issues,
-                                actual_cost,
-                                basis_date: @basis_date,
-                                forecast: @forecast,
-                                etc_method: @calcetc,
-                                no_use_baseline: @no_use_baseline,
-                                working_hours: @working_hours,
-                                region: @region
-    # EVM of versions
-    @version_evm = {}
-    project_version_ids = project_varsion_id_pair @project
-    unless project_version_ids.nil?
-      project_version_ids.each do |proj_id, ver_id|
-        version_issue = version_issues proj_id,
-                                       ver_id
-        version_actual_cost = version_costs proj_id,
-                                            ver_id
-        @version_evm[ver_id] = IssueEvm.new nil,
-                                            version_issue,
-                                            version_actual_cost,
-                                            basis_date: @basis_date,
-                                            forecast: nil,
-                                            etc_method: nil,
-                                            no_use_baseline: true,
-                                            working_hours: @working_hours,
-                                            region: @region
-      end
-    end
-    @no_data = issues.blank?
-    @no_data_incomplete_issues = @incomplete_issues.blank?
-    # export
-    respond_to do |format|
-      format.html
-      format.csv do
-        send_data @project_evm.to_csv,
-                  type: 'text/csv; header=present',
-                  filename: "evm_#{@project.name}_#{Date.current}.csv"
-      end
+      # redirect emv setting
+      redirect_to new_project_evmsetting_path
     end
   end
 
--- app/controllers/evmsettings_controller.rb
+++ app/controllers/evmsettings_controller.rb
@@ -2,6 +2,9 @@
 class EvmsettingsController < ApplicationController
   unloadable
 
+  # Before action
+  before_action :find_project
+
   # Create of baseline
   #
   def new
@@ -25,6 +28,7 @@ class EvmsettingsController < ApplicationController
   def update
     evm_settings = Evmsetting.find(params[:id])
     evm_settings.update_attributes(evm_baseline_params)
+    evm_settings.user_id = User.current.id
     if evm_settings.save
       flash[:notice] = l(:notice_successful_update)
       redirect_to action: :new
@@ -37,22 +41,41 @@ class EvmsettingsController < ApplicationController
   #
   def create
     evm_settings = Evmsetting.new(evm_setting_params)
+    evm_settings.project_id = @project.id
+    evm_settings.user_id = User.current.id
     # Save
-    if evm_settings.save
+    if evm_settings.save!
       flash[:notice] = l(:notice_successful_create)
-      redirect_to action: :index
+      Rails.logger.info(project_evms_path)
+      redirect_to project_evms_path
     else
-      redirect_to action: :new
+      render_404
     end
   end
 
   private
 
-    # Strong parameter
-    #
-    def evm_setting_params
-      params.require(:evmsetting).permit(:view_forecast, 
-                                         :description)
-    end
+  # find project object
+  #
+  def find_project
+    @project = Project.find(params[:project_id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
 
+  # Strong parameter
+  #
+  def evm_setting_params
+    params.require(:evmsetting)
+          .permit(:view_forecast,
+                  :view_version,
+                  :view_performance,
+                  :view_issuelist,
+                  :basis_hours,
+                  :etc_method,
+                  :region,
+                  :threshold_spi,
+                  :threshold_cpi,
+                  :threshold_cr)
+  end
 end
