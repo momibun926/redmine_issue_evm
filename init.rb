d022b4fd0e63d5f50af4aa9341a9aad34cff6fee
Najime Nakagama <hajime.nakagama@gmail.com>
2020-04-07 19:35:12 +0900

    Version up 4.0.1-> 4.0.2

--- init.rb
+++ init.rb
@@ -17,7 +17,7 @@ Redmine::Plugin.register :redmine_issue_evm do
   name "Redmine Issue Evm plugin"
   author "Hajime Nakagama"
   description "Earned value management calculation plugin."
-  version "4.0.1"
+  version "4.0.2"
   url "https://github.com/momibun926/redmine_issue_evm"
   author_url "https://github.com/momibun926"
   project_module :Issuevm do
