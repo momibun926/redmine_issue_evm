70f9e2514dc1843d702c358f733272fecadc0d8c
momibun926 <hajime.nakagama@gmail.com>
2020-04-07 18:31:45 +0900

    Expand chart area

--- lib/chart_data_maker.rb
+++ lib/chart_data_maker.rb
@@ -143,7 +143,7 @@ module ChartDataMaker
       max_date << evm.ev.cumulative.keys.max
       max_date << evm.ac.cumulative.keys.max
     end
-    duration[:end_date] = max_date.max
+    duration[:end_date] = max_date.max + 1
     duration
   end
 
