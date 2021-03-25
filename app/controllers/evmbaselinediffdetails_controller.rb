54b17ab38183418882450de1fe7b96931c33a060
Hajime Nakagama <hajime.nakagama@gmail.com]>
2021-03-25 20:15:59 +0900

    debug code

--- app/controllers/evmbaselinediffdetails_controller.rb
+++ app/controllers/evmbaselinediffdetails_controller.rb
@@ -10,13 +10,13 @@ class EvmbaselinediffdetailsController < BaseevmController
   #
   def index
     # baseline
-    baselines = project_baseline @cfg_param[:baseline_id]
+    baselines = project_baseline params[:baseline_id]
     # issues of project include disendants
     issues = evm_issues @project
     # issue detail
-    issue_detail = create_issue_detail_data issues
+    @issue_detail = create_issue_detail_data issues
     # baseline detail
-    baseline_detail = create_baseline_detail_data baselines
+    @baseline_detail = create_baseline_detail_data baselines
   end
 
   private
@@ -43,12 +43,12 @@ class EvmbaselinediffdetailsController < BaseevmController
   # @return [hash] detail of baseline.
   def create_baseline_detail_data(baselines)
     temp = {}
-    baselies.each do |baseline|
+    baselines.each do |baseline|
       bl_detail = {}
       bl_detail[:start_date] = baseline.start_date
       bl_detail[:due_date] = baseline.due_date
       bl_detail[:estimated_hours] = baseline.estimated_hours
-      teml[baselie.issue_id] = bl_detail
+      temp[baseline.issue_id] = bl_detail
     end
     temp
   end
