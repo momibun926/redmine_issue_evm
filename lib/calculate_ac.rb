6ed776fc5c710f9c9ce7eab6b70882f1820444f4
Hajime Nakagama <hajime.nakagama@gmail.com>
2021-07-04 13:40:07 +0900

    code refactoring

--- lib/calculate_ac.rb
+++ lib/calculate_ac.rb
@@ -16,8 +16,7 @@ module CalculateEvmLogic
     # @param [Date] basis_date basis date.
     # @param [costs] costs culculation of AC.
     def initialize(basis_date, costs)
-      # basis date
-      @basis_date = basis_date
+      super(basis_date)
       # daily AC
       @daily = Hash[costs]
       # minimum first date
