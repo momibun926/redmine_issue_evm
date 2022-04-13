e6cbca45e9e134c5b6a86ce48231c251fc23c84f
Hajime Nakagama <hajime.nakagama@gmail.com>
2022-04-13 12:11:53 +0900

    Change layout

--- app/helpers/evmreports_helper.rb
+++ app/helpers/evmreports_helper.rb
@@ -9,4 +9,13 @@ module EvmreportsHelper
   def baseline_name(baseline)
     baseline.present? ? baseline.subject : ""
   end
+
+  # EVM Difference
+  #
+  # @param [object] evm projectevmreport
+  # @param [object] evm_prev projectevmreport(previusly)
+  # @return [String] Difference
+  def evm_report_difference(report_value, prev_report_value)
+    prev_report_value.nil? ? "" : (report_value - prev_report_value).to_f.round(1)
+  end
 end
