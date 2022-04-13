0088af81a9a1cc568e8adfcf0c7fa4f34915d8f2
Hajime Nakagama <hajime.nakagama@gmail.com>
2022-04-13 12:59:12 +0900

    Correctly calculate difference workingDays

--- app/helpers/evmreports_helper.rb
+++ app/helpers/evmreports_helper.rb
@@ -1,13 +1,22 @@
 # reports helper
 module EvmreportsHelper
   include CommonHelper
-
+  include EvmUtil
   # Get baseline name
   #
   # @param [object] baseline baseline object
   # @return [String] baseline subject
   def baseline_name(baseline)
     baseline.present? ? baseline.subject : ""
+  end
+
+  # EVM(Status date) Difference
+  #
+  # @param [object] evm projectevmreport
+  # @param [object] evm_prev projectevmreport(previusly)
+  # @return [String] Difference
+  def evm_report_status_date_difference(status_date, prev_status_date, exclude_holiday, region)
+    working_days(prev_status_date.to_date, status_date.to_date, exclude_holiday, region).size - 1 unless prev_status_date.nil?
   end
 
   # EVM Difference
