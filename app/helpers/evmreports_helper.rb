a1e1c78e277cafb6f8b1abb5a64a5006e601b825
Hajime Nakagama <hajime.nakagama@gmail.com>
2022-04-17 01:25:50 +0900

    Bug

--- app/helpers/evmreports_helper.rb
+++ app/helpers/evmreports_helper.rb
@@ -16,7 +16,7 @@ module EvmreportsHelper
   # @param [object] evm_prev projectevmreport(previusly)
   # @return [String] Difference
   def evm_report_status_date_difference(status_date, prev_status_date, exclude_holiday, region)
-    working_days(prev_status_date.to_date, status_date.to_date, exclude_holiday, region).size - 1 unless prev_status_date.nil?
+    working_days(prev_status_date.to_date, status_date.to_date, exclude_holiday, region).size unless prev_status_date.nil?
   end
 
   # EVM Difference
