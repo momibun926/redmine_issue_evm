fac962fa92677b352b1b2c8f51d31ba8e9ef5300
Hajime Nakagama <hajime.nakagama@gmail.com>
2022-05-16 05:21:23 +0000

    Some bugfix

--- app/helpers/evmreports_helper.rb
+++ app/helpers/evmreports_helper.rb
@@ -12,18 +12,20 @@ module EvmreportsHelper
 
   # EVM(Status date) Difference
   #
-  # @param [object] evm projectevmreport
-  # @param [object] evm_prev projectevmreport(previusly)
-  # @return [String] Difference
+  # @param [String] status_date report date
+  # @param [String] prev_status_date previusly reported date
+  # @param [String] exclude_holiday include weekend and holiday?
+  # @param [String] region region
+  # @return [Array] working days
   def evm_report_status_date_difference(status_date, prev_status_date, exclude_holiday, region)
     working_days(prev_status_date.to_date, status_date.to_date, exclude_holiday, region).size unless prev_status_date.nil?
   end
 
   # EVM Difference
   #
-  # @param [object] evm projectevmreport
-  # @param [object] evm_prev projectevmreport(previusly)
-  # @return [String] Difference
+  # @param [Numeric] report_value reported value
+  # @param [Numeric] prev_report_value previusly reported value
+  # @return [Numeric] Difference
   def evm_report_difference(report_value, prev_report_value)
     prev_report_value.nil? ? "" : (report_value - prev_report_value).to_f.round(1)
   end
